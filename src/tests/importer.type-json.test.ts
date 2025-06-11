b20516c17d457d2f5a896a48083792fe2651d4ad
nWacky <gosha.evtushenko@gmail.com>
2025-06-11 15:33:05 +0300

    undo the 'imports maps' fix
    decided to make key stringification as an option.
    in case this behavior is desired

--- src/tests/importer.type-json.test.ts
+++ src/tests/importer.type-json.test.ts
@@ -64,10 +64,9 @@ describe('Import type test (JSON)', () => {
 
 	it('imports maps', async () => {
 		const result = await compiler.compileStringAsync(
-			`@use 'sass:map'; @import "maps.json"; body { color: map.get($colors, "red"); }`,
+			`@use 'sass:map'; @import "maps.json"; body { color: map.get($colors, red); }`,
 			sassOptions,
 		);
-
 		expect(result.css.toString()).toContain(expectedResult);
 	});
 
